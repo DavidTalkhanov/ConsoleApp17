f7ba8ab29a349fcb57cceff59cc3063e68739186
Наниашвили Вероника <jackpot7815RUS@mail.ru>
2022-11-28 11:06:53 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp17/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ConsoleApp17
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //константный массив
+            int[] mas = { 5, 8, 1, 9, 78 };
+            foreach (var item in mas)
+                Console.WriteLine(item);
+        }
+    }
+}
